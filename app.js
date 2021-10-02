8bcc941ac8b3b1090ccab16ef29622b9238396eb
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-02 22:59:01 +0900

    [Add]変数

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('Hello World');
+let hello = 'Hello World';
+
+let int1 = 1;
+
+let int2 = -10;
+
+let float1 = 3.14;
+
+let str1 = 'JavaScriptを覚えよう';
+
+alert(hello); alert(int1); alert(int2); alert(float1); alert(str1);
