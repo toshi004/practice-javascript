d803f9f623e6511f32482aa3c162ebc4dbb8a279
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-05 22:53:52 +0900

    四則演算

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ let float1 = 3.14;
 
 let str1 = 'JavaScriptを覚えよう';
 
-alert(hello); alert(int1); alert(int2); alert(float1); alert(str1);
+// alert(hello); alert(int1); alert(int2); alert(float1); alert(str1);
+
+alert(4 + 3);
+
+alert(8 - 5);
+
+alert(2 * 6);
+
+alert(10 / 2);
