8ffb6285b0c3b9dbd1183893ff103abf1741a2c8
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-05 23:13:51 +0900

    [Add]条件分岐

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-let hello = 'Hello World';
-
-let int1 = 1;
-
-let int2 = -10;
-
-let float1 = 3.14;
-
-let str1 = 'JavaScriptを覚えよう';
+/* 変数 */
+// let hello = 'Hello World';
+// let int1 = 1;
+// let int2 = -10;
+// let float1 = 3.14;
+// let str = 'JavaScriptを覚えよう';
 
 // alert(hello); alert(int1); alert(int2); alert(float1); alert(str1);
 
+/* 四則演算 */
 // alert(4 + 3);
-
 // alert(8 - 5);
-
 // alert(2 * 6);
-
 // alert(10 / 2);
 
-alert('Hello' + 'World');
+/* 文字列の連結 */
+// alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+
+alert(str1 + str2);
+
+/* 条件分岐 */
+
+let orange = 100;
+let apple = 100;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+}else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
