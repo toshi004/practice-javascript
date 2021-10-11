e6e1959f937d981252282e99be651b9cc177f9e1
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-11 22:28:39 +0900

    [Add]jQueryアニメーション

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,40 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function() {
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// })
+
+
+/* 上から下にスライド */
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+/* 下から上にスライド */
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
+$(function(){
+  $('.box1').slideDown(1000, function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '400px'
+    }).slideUp(2000);
+  });
 });
