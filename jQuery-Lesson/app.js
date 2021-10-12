38f8d961085d08a4d21664e567bfb3967750bb66
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-10-12 22:35:51 +0900

    [Add]マウスイベント

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -29,12 +29,39 @@
 //   $('.box1').hide();
 // });
 
+// $(function(){
+//   $('.box1').slideDown(1000, function(){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '400px'
+//     }).slideUp(2000);
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000ff'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#ff0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(1000, function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '400px'
-    }).slideUp(2000);
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
